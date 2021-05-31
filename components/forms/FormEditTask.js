63e4b9270e1957c6e5278dddaacf41f9f2c0fded
Andy Smith <work@andysmith.is>
2021-06-01 09:39:44 +1000

    added editable tasks and mutation

--- components/forms/FormEditTask.js
+++ components/forms/FormEditTask.js
@@ -1,28 +1,49 @@
-import { Flex, FormControl, FormLabel, Input } from '@chakra-ui/react';
+import { useAuth } from '@/lib/auth';
+import { editTask } from '@/lib/db';
+import { Button, Flex, FormControl, FormLabel, Input, useToast } from '@chakra-ui/react';
+import { useEffect } from 'react';
 
 import ReactDatePicker from 'react-datepicker';
 import { Controller, useForm } from 'react-hook-form';
+import { mutate } from 'swr';
 
-export default function EditTaskForm({ preloadedValues }) {
-  const { register, handleSubmit, reset, setFocus, control } = useForm({
+const EditTaskForm = ({ preloadedValues }) => {
+  const { user } = useAuth();
+  const toast = useToast();
+
+  const { register, handleSubmit, setValue, reset, setFocus, control } = useForm({
     defaultValues: preloadedValues,
   });
   const handleEditTask = (formData) => {
-    const hash = randomHash(20);
     const dateDue = !formData.dateDue ? null : new Date(formData.dateDue).toISOString();
     const newTaskDetail = {
-      taskId: hash,
-      uid,
-      dateCreated: new Date().toISOString(),
-      projectId,
+      ...preloadedValues,
       dateDue: dateDue,
-      notes: 'fake notes',
-      taskName: formData.taskName,
-      isCompleted: false,
+      notes: formData.notes,
     };
+    editTask(newTaskDetail);
+    toast({
+      title: 'Task Edited.',
+      description: 'Open the Task to add a list of items',
+      status: 'success',
+      duration: 3000,
+      isClosable: true,
+    });
+
+    mutate(
+      ['/api/tasks', user.token],
+      async (data) => {
+        console.log('newTaskDetail', newTaskDetail);
+        const newData = data.tasks.map(
+          (task) => [newTaskDetail].find((o) => o.id === task.id) || task,
+        );
+        return { tasks: [...newData] };
+      },
+      false,
+    );
   };
   return (
-    <FormControl onSubmit={handleEditTask}>
+    <FormControl as='form' onSubmit={handleSubmit(handleEditTask)}>
       <Flex
         display='flex'
         flexDirection='column'
@@ -35,19 +56,18 @@ export default function EditTaskForm({ preloadedValues }) {
           Due Date
         </FormLabel>
         <Controller
-          {...register('dateDue')}
           _hover={{ cursor: 'pointer' }}
           control={control}
           name='dateDue'
-          render={({ field: { onChange, onBlur, value } }) => (
+          render={({ field: { onChange, onBlur, value, ref } }) => (
             <ReactDatePicker
               onChange={onChange}
               variant='unstyled'
+              // inputRef={ref}
               onBlur={onBlur}
               selected={!value ? value : new Date(value)}
               dateFormat='dd MMM yyyy'
               shouldCloseOnSelect
-              calendarIcon='calendar'
               placeholderText={'Select a date'}
               showTimeSelect={false}
               todayButton='Today'
@@ -74,6 +94,13 @@ export default function EditTaskForm({ preloadedValues }) {
           {...register('notes')}
         />
       </Flex>
+      <Flex mt={4}>
+        <Button w='55%' type='submit' colorScheme='green'>
+          Update
+        </Button>
+      </Flex>
     </FormControl>
   );
-}
+};
+
+export default EditTaskForm;
